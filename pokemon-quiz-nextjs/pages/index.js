6dd2e8edcbe2127056fb494b8ee0282cfd36366b
Douglas Marcelino <33377432+TheeDouglasAM3@users.noreply.github.com>
2021-01-25 16:57:31 -0300

    Update page/index.js

--- pokemon-quiz-nextjs/pages/index.js
+++ pokemon-quiz-nextjs/pages/index.js
@@ -1,10 +1,49 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import QuizLogo from '../src/components/QuizLogo'
+import QuizBackground from '../src/components/QuizBackground'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 5%;
+  float: right;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding-top: 15px;
+    float: none;
+  }
+`;
+
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/theedouglasam3" />
+    </QuizBackground>
+  )
 }
