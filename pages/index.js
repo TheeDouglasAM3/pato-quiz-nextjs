01b860eb4684c009af9699e9721c2dbbf5fe9787
Douglas Marcelino <33377432+TheeDouglasAM3@users.noreply.github.com>
2021-01-25 20:07:13 -0300

    Update context of project

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,10 @@ export const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
   padding-top: 45px;
-  margin: auto 5%;
-  float: right;
+  margin: auto 10%;
   @media screen and (max-width: 500px) {
     margin: auto;
     padding-top: 15px;
-    float: none;
   }
 `;
 
