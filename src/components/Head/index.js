6b75807fd214bd37323c53bdbd9f2cdd23353f92
Douglas Marcelino <33377432+TheeDouglasAM3@users.noreply.github.com>
2021-01-25 22:18:29 -0300

    Add more meta tag in Head component

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -6,10 +6,17 @@ export default function Home({ title, bg }) {
         <title>{title}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:locale" content="pt_BR" />
+
+        <meta property="og:title" content={title} />
+        <meta property="og:site_name" content="PatoQuiz" />
+
+        <meta property="og:description" content="Minha boa descrição para intrigar os usuários." />
         <meta property="og:image" content={bg} />
         <meta property="og:image:type" content="image/jpg" />
-        <meta property="og:image:width" content="800" />
-        <meta property="og:image:height" content="600" />
+        <meta property="og:image:width" content="1920" />
+        <meta property="og:image:height" content="1080" />
+        <meta property="og:type" content="website"></meta>
       </Head>
   )
 }
