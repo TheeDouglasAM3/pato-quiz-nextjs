59bf198e0146d207007b9b38f80ad522b71cf74b
Douglas Marcelino <33377432+TheeDouglasAM3@users.noreply.github.com>
2021-01-25 22:10:48 -0300

    Create Head component with image meta property

---
+++ src/components/Head/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home({ title, bg }) {
+  return (
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:image" content={bg} />
+        <meta property="og:image:type" content="image/jpg" />
+        <meta property="og:image:width" content="800" />
+        <meta property="og:image:height" content="600" />
+      </Head>
+  )
+}
